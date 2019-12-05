35b40d8a1950c825c97edd92dcf182509315b615
cycmay <1769614470@qq.com>
2019-12-05 16:34:54 +0800

    :fix some

--- vehicleBrand/settings/production.py
+++ vehicleBrand/settings/production.py
@@ -5,4 +5,4 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['62.234.141.226', "127.0.0.1"]
+ALLOWED_HOSTS = ['106.53.20.146', "127.0.0.1"]
